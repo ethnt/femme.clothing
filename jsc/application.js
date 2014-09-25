406a0d38bd52f9765db73ae9c0969f207e8d35bd
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-09-25 19:03:37 -0400

    single post pages with a slideshow.

--- jsc/application.js
+++ jsc/application.js
@@ -1,4 +1,54 @@
 (function() {
-  $(document).ready(function() {});
+  var Slideshow,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  Slideshow = (function() {
+    function Slideshow(container) {
+      this.container = container;
+      this.next = __bind(this.next, this);
+      this.previous = __bind(this.previous, this);
+      this.refresh = __bind(this.refresh, this);
+      this.index = 0;
+      this.images = $(this.container).children("img");
+      this.refresh();
+    }
+
+    Slideshow.prototype.refresh = function() {
+      var current;
+      this.images.hide();
+      current = $(this.images[this.index]);
+      return current.show();
+    };
+
+    Slideshow.prototype.previous = function() {
+      this.index = this.index - 1;
+      if (this.index < 0) {
+        this.index = this.images.size() - 1;
+      }
+      return this.refresh();
+    };
+
+    Slideshow.prototype.next = function() {
+      this.index = this.index + 1;
+      if (this.index === this.images.size()) {
+        this.index = 0;
+      }
+      return this.refresh();
+    };
+
+    return Slideshow;
+
+  })();
+
+  $(document).ready(function() {
+    var slideshow;
+    slideshow = new Slideshow(".slides");
+    $(".previous").on("click", function() {
+      return slideshow.previous();
+    });
+    return $(".next").on("click", function() {
+      return slideshow.next();
+    });
+  });
 
 }).call(this);
--- jsc/src/application.coffee
+++ jsc/src/application.coffee
@@ -1,5 +1,46 @@
 # Femme.
 #   file: jsc/src/application.coffee
 
+class Slideshow
+  constructor: (@container) ->
+    @index = 0
+    @images = $(@container).children("img")
+
+    @.refresh()
+
+
+  refresh: =>
+    # Hide all of the images to start.
+    @images.hide()
+
+    # Show the image at the current index.
+    current = $(@images[@index])
+    current.show()
+
+
+  previous: =>
+    @index = @index - 1
+
+    if @index < 0
+      @index = @images.size() - 1
+
+    @.refresh()
+
+
+  next: =>
+    @index = @index + 1
+
+    if @index == @images.size()
+      @index = 0
+
+    @.refresh()
+
+
 $(document).ready ->
-  
+  slideshow = new Slideshow(".slides")
+
+  $(".previous").on "click", ->
+    slideshow.previous()
+
+  $(".next").on "click", ->
+    slideshow.next()
